057f2a6e73259272aad2a4cf292462649a01309e
samson <samson.omondi1@ibm.com>
2023-10-30 13:32:24 +0300

    add styles to react

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -14,16 +14,48 @@ import '../box-styles.css'
 // 🐨 also use the style prop to make the font italic
 // 💰 Here are available style attributes: backgroundColor, fontStyle
 
-const smallBox = <div>small lightblue box</div>
-const mediumBox = <div>medium pink box</div>
-const largeBox = <div>large orange box</div>
+// const smallBox = (
+//   <div className="box box--small" style={{backgroundColor: 'lightblue'}}>
+//     small lightblue box
+//   </div>
+// )
+// const mediumBox = (
+//   <div className="box box--medium" style={{backgroundColor: 'pink'}}>
+//     medium pink box
+//   </div>
+// )
+// const largeBox = (
+//   <div className="box box--large" style={{backgroundColor: 'orange'}}>
+//     large orange box
+//   </div>
+// )
+
+// using custom component
+function Box(props) {
+  return <div {...props}>{props.children}</div>
+}
 
 function App() {
   return (
     <div>
-      {smallBox}
-      {mediumBox}
-      {largeBox}
+      <Box
+        className="box box--small"
+        style={{backgroundColor: 'lightblue', fontStyle: 'italic'}}
+      >
+        small lightblue box
+      </Box>
+      <Box
+        className="box box--medium"
+        style={{backgroundColor: 'pink', fontStyle: 'italic'}}
+      >
+        medium pink box
+      </Box>
+      <Box
+        className="box box--large"
+        style={{backgroundColor: 'orange', fontStyle: 'italic'}}
+      >
+        large orange box
+      </Box>
     </div>
   )
 }
